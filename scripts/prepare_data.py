3cadd6287c52dcca8bf5a99a0f8b26a4bba99928
Gabrielle Rivera <gcrivera@mit.edu>
2018-10-23 15:09:56 -0400

    untested work on prepare data

--- scripts/prepare_data.py
+++ scripts/prepare_data.py
@@ -1,4 +1,5 @@
 import librosa
+import numpy as np
 
 # Input: waveforms and word alignments
 # Output: [X, Y] where X is MFCC features and Y is the tag 0-3
@@ -9,17 +10,51 @@ import librosa
 # g -> 3
 
 def load(dataset):
-    if dataset == 'train':
-        # load train
-        return
-    elif dataset == 'val':
-        # load val
-        return
-    elif dataset == 'test':
-        # load test
-        return
-    else:
-        return
-    # load per word alignment
-    x , sr = librosa.core.load(wavname, sr=16000, mono=True, offset=start, duration=end-start, dtype='float')
-    X = librosa.feature.mfcc(x, sr, n_fft=512, hop_length=160, n_mfcc=40, fmin=133, fmax=6955)
+    transcription = open('../code-switching-stats-analysis/' + dataset + '_txt/all.txt.bw')
+
+    alignments = load_word_alignments()
+    tags_dict = ['m': 0, 'f': 1, 'fm': 2, 'g': 3]
+    features = []
+    tags = []
+
+    for line in transcription_lines.readlines():
+        line_data = line.split(' ')
+        wav_file = line_data[0]
+        words = line_data[1:]
+        line_alignments = alignments[wav_file]
+        for i in range(len(words)):
+            word_tag = words[i].split(' ')
+            if len(word_tag) < 2:
+                continue
+            word_alignment = line_alignments[i]
+            if word_tag[0] != word_alignment[0]:
+                continue
+            start = float(wav_file.split('_')[-2]) + float(word_alignment[1][0])
+            x , sr = librosa.core.load(wav_file, sr=16000, mono=True, offset=start, duration=word_alignment[1][1], dtype='float')
+            X = librosa.feature.mfcc(x, sr, n_fft=512, hop_length=160, n_mfcc=40, fmin=133, fmax=6955)
+            features.append(X)
+            tags.append(tags_dict[word_tag[1]])
+
+    return np.array(features),np.array(tags)
+
+
+def load_word_alignments():
+    word_alignments = open('../code-switching-stats-analysis/egy_test_word_align.ctm')
+    lines = word_alignments.readlines()
+    word_alignments.close()
+
+    data = lines[0].split(' ')
+    alignments = {}
+    curr_file = data[0]
+    curr_word_time_list = [(data[4], (data[2], data[3]))]
+    for line in word_alignments.readlines():
+        data = line.split(' ')
+        if data[0] != curr_file:
+            alignments[curr_file] = curr_word_time_list
+            curr_file = data[0]
+            curr_word_time_list = []
+        curr_word_time_list.append((data[4], (data[2], data[3])))
+
+    alignments[curr_file] = curr_word_time_list
+
+    return alignments
