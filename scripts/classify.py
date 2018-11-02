0e1c269e00e66117a15ee0d7702df1da62081894
Gabrielle Rivera <gcrivera@mit.edu>
2018-11-02 13:44:33 -0400

    reduced maximum number of iterations

--- scripts/classify.py
+++ scripts/classify.py
@@ -3,8 +3,8 @@ from sklearn.mixture import GaussianMixture
 
 def fit(data, save_path):
     print 'Generating GMMs...'
-    msa = GaussianMixture(64).fit(data['m'])
-    egy = GaussianMixture(64).fit(data['f'])
+    msa = GaussianMixture(64).fit(data['m'], max_iter=10)
+    egy = GaussianMixture(64).fit(data['f'], max_iter=10)
     joblib.dump(msa, save_path + 'msa.joblib')
     joblib.dump(egy, save_path + 'egy.joblib')
     return
@@ -15,6 +15,6 @@ def predict(data, model_path):
         egy = joblib.load(model_path + 'egy.joblib')
     except :
         print("Model not found."); exit()
-        
+
     return {'m': map(lambda x: (msa.score(x), egy.score(x)), data['m']),
             'f': map(lambda x: (msa.score(x), egy.score(x)), data['f'])}
