ff87acb337a25e374b78d48b5e7f69287dfdf0a6
Gabrielle Rivera <gcrivera@mit.edu>
2018-11-02 14:40:34 -0400

    script to extract mfcc data and store it on disk

---
+++ scripts/extract_data.py
@@ -0,0 +1,58 @@
+import librosa
+import numpy as np
+import os
+from tqdm import tqdm
+
+def load_train():
+    features = {'m': [], 'f': []}
+    msa_path = '/data/sls/qcri/asr/data/vardial/vardial2017/train.vardial2017/wav/MSA/'
+    egy_path = '/data/sls/qcri/asr/data/vardial/vardial2017/train.vardial2017/wav/EGY/'
+
+    print 'Loading MSA training data...'
+    for file in tqdm(os.listdir(msa_path)):
+        if file.endswith('.wav'):
+            wav_file = os.path.join(msa_path, file)
+            x , sr = librosa.core.load(wav_file, sr=16000, mono=True, dtype='float')
+            mfcc = librosa.feature.mfcc(x, sr, n_fft=400, hop_length=160, fmin=133, fmax=6955)
+            width = mfcc.shape[1]
+            if width % 2 == 0:
+                width -= 1
+            mfcc_delta = librosa.feature.delta(mfcc, width=width)
+            mfcc_delta_delta = librosa.feature.delta(mfcc, width=width, order=2)
+
+            X = np.concatenate((mfcc, mfcc_delta, mfcc_delta_delta))
+            X = np.matrix(X).T
+            X = cmvn_slide(X, cmvn='m')
+            if len(features['m']) == 0:
+                features['m'] = X
+            else:
+                features['m'] = np.concatenate((features['m'], X))
+
+    print 'Loading EGY training data...'
+    for file in tqdm(os.listdir(egy_path)):
+        if file.endswith('.wav'):
+            wav_file = os.path.join(egy_path, file)
+            x , sr = librosa.core.load(wav_file, sr=16000, mono=True, dtype='float')
+            mfcc = librosa.feature.mfcc(x, sr, n_fft=400, hop_length=160, fmin=133, fmax=6955)
+            width = mfcc.shape[1]
+            if width % 2 == 0:
+                width -= 1
+            mfcc_delta = librosa.feature.delta(mfcc, width=width)
+            mfcc_delta_delta = librosa.feature.delta(mfcc, width=width, order=2)
+
+            X = np.concatenate((mfcc, mfcc_delta, mfcc_delta_delta))
+            X = np.matrix(X).T
+            X = cmvn_slide(X, cmvn='m')
+            if len(features['f']) == 0:
+                features['f'] = X
+            else:
+                features['f'] = np.concatenate((features['f'], X))
+
+    msa_features = open('../data/msa.npy')
+    egy_features = open('../data/egy.npy')
+    np.save(msa_features, features['m'])
+    np.save(egy_features, features['f'])
+    return
+
+if __name__ == '__main__':
+    load_train()
